1b81799e7702f426d446c78763796722d9a3b883
scottalexgray <scottalexgray@gmail.com>
2022-04-25 13:15:50 +0200

    initial publish

---
+++ src/ubidots_publish_widget.js
@@ -0,0 +1,57 @@
+var TOKEN = "null"; //retrieved at runtime
+
+var ubidots = new Ubidots();
+ubidots.on('receivedToken', function (data)
+{
+	TOKEN = data;
+	console.log("Received Token");
+
+	//try initialise local variables
+	InitialiseElements();	
+});
+
+
+
+function PublishVariable()
+{
+    //default values
+    var deviceApiLabel = "null"; //specified by test input
+    var variableApiLabel = "null"; //specified by test input
+    var variableValue = 0;
+    var variableContext = "";
+
+    //try get values from the DOM
+    deviceApiLabel = document.getElementById('deviceApiLabel').value;
+    variableApiLabel = document.getElementById('variableApiLabel').value;
+    variableValue = document.getElementById('variableValue').value;
+    variableContext = document.getElementById('variableContext').value;
+
+    //print to console
+    console.log("deviceApiLabel: " + deviceApiLabel);
+    console.log("variableApiLabel: " + variableApiLabel);
+    console.log("variableValue: " + variableValue);
+    console.log("variableContext: " + variableContext);
+
+
+    if(deviceApiLabel == "null" || variableApiLabel == "null")
+    {
+        console.log("Device API Label or Variable API Label not set");
+        return;
+    }
+}
+
+
+
+
+
+
+
+
+function UploadVariableToUbidots(variableApiLabel, variableValue, variableContext)
+{
+	var url = "https://industrial.api.ubidots.com/api/v1.6/devices/" + deviceApiLabel + "/";
+	var headers = {'Content-Type' : 'application/json', 'X-Auth-Token' : TOKEN};
+	var data = "{\"" + variableApiLabel + "\": {\"value\": " + variableValue + ", \"context\":" + JSON.stringify(variableContext) + "}}";	
+	
+	$.post({url: url, headers: headers, data: data});
+}
